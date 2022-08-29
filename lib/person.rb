519cbbadc6f97845077d0f24890db3249a1e415a
kumabio <kumabio.prince@student.moringaschool.com>
2022-08-29 10:25:54 +0000

    commit tree fc1393a3b9e649b872baf2c518f2515ea94e4af6

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+    
+    def name=(name)
+        @name = name
+    end
+
+    def breed=(job)
+        @breed = (job)
+    end
+
+     def name 
+        @name
+    end
+
+    def job
+        @job
+    end
+
 
 end
